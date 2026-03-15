a77eaf5c09dec361763fe18d00ad92a883a108f9
= <sojwalpatil2006@gmail.com>
2026-03-16 01:23:13 +0530

    Allowed host.

--- todolist/settings.py
+++ todolist/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-52lptcupi$+39(j&s#4tm$))m*ypsl!_!nm-szyo0aget!%%e$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-list-u29j.onrender.com']
 
 
 # Application definition
